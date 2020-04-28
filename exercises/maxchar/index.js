e7376f7bee56bcff848d7b03990dd849be511bf6
ChangmoSung <changmosung0914@gmail.com>
2020-04-28 02:25:03 -0400

    find the most commonly used character

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,27 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+const str = 'hello thereeeeeee';
+
+function maxChar(str) {
+    const obj = {};
+    let max = 0;
+    let maxChar = '';
+
+    for (let char of str) {
+        obj[char] = obj[char] + 1 || 1;
+    }
+
+    for (let char in obj) {
+        if (obj[char] > max) {
+            max = obj[char];
+            maxChar = char;
+        }
+    }
+
+    return maxChar
+}
+
+maxChar(str)
 
 module.exports = maxChar;
