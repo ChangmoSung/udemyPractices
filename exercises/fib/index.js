e3923028574602f778d3db65a53c4a5c489251eb
ChangmoSung <changmosung0914@gmail.com>
2020-05-02 04:21:11 -0400

    recursive solution

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -9,17 +9,26 @@
 //   fib(4) === 3
 
 
+// function fib(n) {
+    // const result = [0, 1];
+
+    // for (let i = 2; i <= n; i++) {
+    //     const a = result[i - 1];
+    //     const b = result[i - 2];
+
+    //     result.push(a + b);
+    // }
+
+    // return result[n];
+// }
+
+
 function fib(n) {
-    const result = [0, 1];
-
-    for (let i = 2; i <= n; i++) {
-        const a = result[i - 1];
-        const b = result[i - 2];
-
-        result.push(a + b);
+    if (n < 2) {
+        return n;
     }
 
-    return (result);
+    return fib(n - 1) + fib(n - 2);
 }
 
 fib(4);
