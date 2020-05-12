67b4b6d83f6a6a9fd083a31a742878f40b3b9880
ChangmoSung <changmosung0914@gmail.com>
2020-05-12 01:27:48 -0400

    learning linked list

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -3,32 +3,49 @@
 // See 'directions' document
 
 class Node {
-    constructor(data, next = null) {
-        this.data = data;
-        this.next = next;
-    }
-}
-
-class LinkedList {
-    constructor() {
-        this.head = null;
+        constructor(data, next = null) {
+            this.data = data;
+            this.next = next;
+        }
     }
 
-    insertFirst(data) {
-        this.head = new Node(data, this.head);
-    }
-
-    size() {
-        let counter = 0;
-        let node = this.head;
-
-        while (node) {
-            counter++;
-            node = node.next;
+    class LinkedList {
+        constructor() {
+            this.head = null;
         }
 
-        return counter;
+        insertFirst(data) {
+            this.head = new Node(data, this.head);
+        }
+
+        size() {
+            let counter = 0;
+            let node = this.head;
+
+            while(node) {
+                counter++;
+                node = node.next;
+            }
+
+            return counter;
+        }
+
+        getFirst() {
+            return this.head;
+        }
+
+        getLast() {
+            if(!this.head) {
+                return null;
+            }
+
+            let node = this.head;
+            while(node) {
+                if(!node.next) {
+                    return node;
+                }
+            }
+        }
     }
-}
 
 module.exports = { Node, LinkedList };
