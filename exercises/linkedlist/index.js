dfe3fba4d4aca16f8d2acf4aa41f7f72e4435c29
ChangmoSung <changmosung0914@gmail.com>
2020-05-12 01:53:55 -0400

    remove last in linked list

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -3,51 +3,84 @@
 // See 'directions' document
 
 class Node {
-        constructor(data, next = null) {
-            this.data = data;
-            this.next = next;
+    constructor(data, next = null) {
+        this.data = data;
+        this.next = next;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+    insertFirst(data) {
+        this.head = new Node(data, this.head);
+    }
+
+    size() {
+        let counter = 0;
+        let node = this.head;
+
+        while (node) {
+            counter++;
+            node = node.next;
+        }
+
+        return counter;
+    }
+
+    getFirst() {
+        return this.head;
+    }
+
+    getLast() {
+        if (!this.head) {
+            return null;
+        }
+
+        let node = this.head;
+        while (node) {
+            if (!node.next) {
+                return node;
+            }
+
+            node = node.next;
         }
     }
 
-    class LinkedList {
-        constructor() {
+    clear() {
+        this.head = null;
+    }
+
+    removeFirst() {
+        if (!this.head) {
+            return;
+        }
+
+        this.head = this.head.next;
+    }
+
+    removeLast() {
+        if (!this.head) {
+            return;
+        }
+
+        if (!this.head.next) {
             this.head = null;
+            return;
         }
 
-        insertFirst(data) {
-            this.head = new Node(data, this.head);
+        let previous = this.head;
+        let node = this.head.next;
+
+        while (node.next) {
+            previous = node;
+            node = node.next;
         }
 
-        size() {
-            let counter = 0;
-            let node = this.head;
-
-            while(node) {
-                counter++;
-                node = node.next;
-            }
-
-            return counter;
-        }
-
-        getFirst() {
-            return this.head;
-        }
-
-        getLast() {
-            if(!this.head) {
-                return null;
-            }
-
-            let node = this.head;
-            while(node) {
-                if(!node.next) {
-                    return node;
-                }
-
-                node = node.next;
-            }
-        }
+        previous.next = null;
     }
+}
 
 module.exports = { Node, LinkedList };
